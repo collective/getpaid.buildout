3e2aa2b499ee5284c7d8d571ae4c07098aec550c
jpg.rcom <jpg.rcom@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2009-10-22 15:30:20 +0000

    Cleaning up...

--- src/getpaid.nmi/src/getpaid/nmi/browser/button.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) 2007 ifPeople, Juan Pablo Giménez, and Contributors
-
-from Products.Five.browser import BrowserView
-
-class View(BrowserView):
-    pass
--- src/getpaid.nmi/src/getpaid/nmi/vocabulary.py
+++
@@ -1,1 +0,0 @@
-# Copyright (c) 2007 ifPeople, Kapil Thangavelu, and Contributors
